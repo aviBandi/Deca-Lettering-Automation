71b5cf890936492fad89575a5497aaff7ad43ef7
aviBandi <140932033+aviBandi@users.noreply.github.com>
2024-04-19 14:35:20 -0500

    added more names to execution and the output looks better

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 names = ['Avi Bandi', 'Varun Nagapakar', 'Sai Kimbler', "Rahul Billakanti", "Paul Kimbler", "Avinash Bandi",
          "Lexi Shasher", "Michelle Jacklitch", "Michael Lindsay", "Rowan Huh", "Aman Syaed", "Cole Paul Ledman",
          "Prohitt Ram Kumar", "Scott Gengler", "Brooks Gengler", "Kavin Gunnalseeland", "Viswas Valla",
-         "Rukshan Rajan", "Dilshan Rajan", "Jack Berge", "Daniel Salantino", "Aaron Beduhn", "Tika Kude"]
+         "Rukshan Rajan", "Dilshan Rajan", "Jack Berge", "Daniel Salantino", "Aaron Beduhn", "Tika Kude",
+         "Jacklitch Kude", "Fan Boy" "Liam Hall", "Drew Nepsted", "Will Weber", "Amogh Rajgopal", "Maddy Vickers",
+         "Robyn Van Horn", "Pranav Gangireddy", "Nithya Dharmagari", "Ashwanth Vishwanathan", "Ruan Paunland",
+         "Michael Shumaker"]
 
-print(len(names))
+print("Letters to generate:", len(names))
 
 from PyPDF2 import PdfReader, PdfWriter
 from reportlab.lib.pagesizes import letter
@@ -15,6 +18,9 @@ from reportlab.pdfgen import canvas
 from reportlab.lib import colors
 import io
 import os
+import time
+
+start_time = time.time()
 
 output_folder = "letteringPDFs"
 
@@ -62,3 +68,9 @@ for each in names:
     add_words_to_pdf("data/blankDoc.pdf", each.split()[-1] + each.split()[0] + "Lettering" + ".pdf", each, (310, 600))
     add_words_to_pdf(os.path.join(output_folder,each.split()[-1]+each.split()[0])+"Lettering"+".pdf", each.split()[-1]+each.split()[0]+"Lettering"+".pdf", "Wayzata DECA", (310, 300))
     print("Letter created for: ", each)
+
+
+
+end_time = time.time()
+print("\n")
+print("Execution time: ", end_time-start_time, "Seconds")
